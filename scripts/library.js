35cfcd92e65ea015b081a3fe12c91d9d54fbdb81
Jsalvadorpp <josesalvadorpp@hotmail.com>
2020-05-26 18:21:45 -0400

    user can add books

--- scripts/library.js
+++ scripts/library.js
@@ -22,3 +22,59 @@ window.addEventListener('click',(e)=>{
     } 
 });
 //$ =============================================
+
+
+//$ ======== Books collection ==================
+let myLibrary = [];
+
+
+//$ ============================================
+
+
+//$ ======== Add new book  =====================
+let addBookBtn = document.getElementById('add-book');
+let form = document.getElementById('book-form');
+
+addBookBtn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    addBookToLibrary();
+});
+
+class Book{
+    constructor(title,author,status){
+        this.title = title;
+        this.author = author;
+        this.status = status;
+    }
+}
+
+function addBookToLibrary(){
+
+    let data = new FormData(form);
+    let book = new Book(data.get('title'),data.get('author'),data.get('status'));
+    myLibrary.push(book);
+
+    let libraryWrapper = document.getElementById('library');
+    let bookCard = createBookCard(book);
+
+    libraryWrapper.appendChild(bookCard);
+}
+
+function createBookCard(book){
+
+    let template = document.getElementById('card-template');
+    let bookCard = template.cloneNode(true);
+    bookCard.removeAttribute('id');
+
+    bookCard.children[0].textContent = book.title;
+    bookCard.children[1].textContent += book.author;
+    bookCard.children[2].textContent = book.status;
+    bookCard.children[2].classList.add(book.status);
+
+    return bookCard;
+}
+//$ ============================================
+
+
+
+
